77794cb75265f2047ef7bb4412fdd13c38b10699
Uliana Tomyn <ulyatomyn@gmail.com>
2020-07-31 01:33:04 +0300

    changed application.properties, pom, Application, added ProgressRepositoryTest, StudentTest, UserRepositoryTest

---
+++ src/test/java/com.sprint.hibernate/UserRepositoryTest.java
@@ -0,0 +1,136 @@
+package com.sprint.hibernate;
+
+import com.sprint.hibernate.entity.Marathon;
+import com.sprint.hibernate.entity.User;
+import com.sprint.hibernate.repository.MarathonRepository;
+import com.sprint.hibernate.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.List;
+
+@DataJpaTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private MarathonRepository marathonRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;               //????????????????
+
+    private static User student1;
+    private static User student2;
+    private static User student3;
+    private static User mentor1;
+    private static User mentor2;
+    private static Marathon marathon1;
+
+    @BeforeAll
+    public void setUp() {
+        marathon1 = Marathon.builder().id(1).title("JOM_1").build();
+        marathonRepository.save(marathon1);
+        student1 = User.builder().firstName("Uliana")
+                .id(1)
+                .lastName("Tomyn")
+                .email("uliana@gmail.com")
+                .password("password1")
+                .role(User.Role.TRAINEE)
+                .build();
+        student2 = User.builder()
+                .id(2)
+                .firstName("Andriy")
+                .lastName("Lagun")
+                .email("andriy@gmail.com")
+                .password("password2")
+                .role(User.Role.TRAINEE)
+                .build();
+        student3 = User.builder()
+                .id(3)
+                .firstName("Olesia")
+                .lastName("Setrina")
+                .email("olesia@gmail.com")
+                .password("password3")
+                .role(User.Role.TRAINEE)
+                .build();
+        mentor1 = User.builder()
+                .id(4)
+                .firstName("Mykola")
+                .lastName("Demchyna")
+                .email("mykola@gmail.com")
+                .password("password4")
+                .role(User.Role.MENTOR)
+                .build();
+        mentor2 = User.builder()
+                .id(5)
+                .firstName("Nataliia")
+                .lastName("Romanenko")
+                .email("nataliia@gmail.com")
+                .password("password5")
+                .role(User.Role.MENTOR)
+                .build();
+
+        student1.setMarathons(List.of(marathon1));
+        student2.setMarathons(List.of(marathon1));
+        mentor1.setMarathons(List.of(marathon1));
+
+
+//        entityManager.persist(student1);
+        userRepository.save(student1);
+        userRepository.save(student2);
+        userRepository.save(student3);
+        userRepository.save(mentor1);
+        userRepository.save(mentor2);
+        marathon1.setUsers(List.of(student1, student2, mentor1));
+        marathonRepository.save(marathon1);
+    }
+
+    @Test
+    public void newUserTest() {
+        User user = User.builder()
+                .role(User.Role.valueOf("TRAINEE"))
+                .email("roman@i.ua")
+                .firstName("Roman")
+                .lastName("Ivaniuk")
+                .password("15247")
+                .build();
+        userRepository.save(user);
+
+        User actual = userRepository.findUserByEmail("roman@i.ua");
+
+        Assertions.assertEquals("Roman", actual.getFirstName());
+
+    }
+
+    @Test
+    public void getAllStudentsTest() {
+        String expected = List.of(student1, student2, student3).toString();
+        String actual = userRepository.getAllByRole(User.Role.valueOf("TRAINEE")).toString();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findAllByMarathonsIdAndRoleTest() {
+        String expected = List.of(student1, student2).toString();
+        String actual = userRepository.findAllByMarathonsIdAndRole(marathon1.getId(), User.Role.valueOf("TRAINEE")).toString();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void deleteStudentTest() {
+        String expected = List.of(student1, student2).toString();
+        userRepository.deleteById(student3.getId());
+        String actual = userRepository.getAllByRole(User.Role.valueOf("TRAINEE")).toString();
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+}
